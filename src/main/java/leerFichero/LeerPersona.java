694d1a4a414d4a55bf703a82117d006efcb3f44c
Abel <avazquez18@jaumebalmes.net>
2019-10-25 23:38:42 +0200

    mejorada funcion escapar comas, aunque si el usuario introduce dos comas seguidas tal que así ,, la funcion comprobarComas de la clase EscribirPersona las traduce como  \ por lo cual la funcion devolverEstadoOriginal de la clase LeerPersona traduce esta doble barra invertida en

--- src/main/java/leerFichero/LeerPersona.java
+++ src/main/java/leerFichero/LeerPersona.java
@@ -5,27 +5,27 @@ import java.util.ArrayList;
 
 public class LeerPersona {
     public static void main(String[] args) {
-        ArrayList<String []> listaPersonas = new ArrayList<>();
-        File f = new File("/home/abel/IdeaProjects/primerProject/crearPersonas.txt");
+        ArrayList<String[]> listaPersonas = new ArrayList<>();
+        File f = new File("crearPersonas.txt");
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
-            String [] infoPersona;
+            String[] infoPersona;
             String lineaInfoPersona;
-            while ((lineaInfoPersona = bufferedReader.readLine())!=null){
-                infoPersona=lineaInfoPersona.split(",");
+            while ((lineaInfoPersona = bufferedReader.readLine()) != null) {
+                infoPersona = lineaInfoPersona.split(",");
                 devolverEstadoOriginal(infoPersona);
                 listaPersonas.add(infoPersona);
             }
-            for (String[] infoPersonas:listaPersonas) {
+            for (String[] infoPersonas : listaPersonas) {
                 System.out.println("-----------------------------------------");
-                System.out.println("Persona numero "+((int)listaPersonas.indexOf(infoPersonas)+1));
+                System.out.println("Persona numero " + ((int) listaPersonas.indexOf(infoPersonas) + 1));
                 System.out.println("-----------------------------------------");
-                System.out.println("Nombre: "+infoPersonas[0]);
-                System.out.println("Apellidos: "+infoPersonas[1]);
-                System.out.println("DNI: "+infoPersonas[2]);
-                System.out.println("Direccion: "+infoPersonas[3]);
-                System.out.println("Poblacion: "+infoPersonas[4]);
-                System.out.println("Codigo Postal: "+infoPersonas[5]);
+                System.out.println("Nombre: " + infoPersonas[0]);
+                System.out.println("Apellidos: " + infoPersonas[1]);
+                System.out.println("DNI: " + infoPersonas[2]);
+                System.out.println("Direccion: " + infoPersonas[3]);
+                System.out.println("Poblacion: " + infoPersonas[4]);
+                System.out.println("Codigo Postal: " + infoPersonas[5]);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -33,23 +33,23 @@ public class LeerPersona {
             e.printStackTrace();
         }
     }
-    public static void devolverEstadoOriginal (String[]lista){
-        for (int i = 0; i <lista.length ; i++) {
-            String modificat="";
+
+    public static void devolverEstadoOriginal(String[] lista) {
+        for (int i = 0; i < lista.length; i++) {
+            String modificat = "";
             for (int j = 0; j < lista[i].length(); j++) {
-                if (lista[i].charAt(j)=='\\'){
-                    if ((j>lista[i].length())&&!(lista[i].charAt((j)+1)=='\\')){
-                        modificat=modificat+',';
+                if (lista[i].charAt(j) == '\\') {
+                    if ((j<lista[i].length()-1)&&lista[i].charAt((j) + 1) == '\\') {
+                        modificat = modificat + "\\";
+                        j++;
+                    } else {
+                        modificat = modificat + ",";
                     }
-                    else {
-                        modificat=modificat+'\\';
-                    }
-                }
-                else {
-                    modificat=modificat+lista[i].charAt(j);
+                } else {
+                    modificat = modificat + lista[i].charAt(j);
                 }
             }
-            lista[i]=modificat;
+            lista[i] = modificat;
         }
     }
 }
